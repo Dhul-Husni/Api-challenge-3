319aecf4a00be59e26564a8f357f0a700eb83253
sir3n-sn <sir3n.sn@gmail.com>
2017-12-03 16:59:25 +0300

    .

--- instance/config.py
+++ instance/config.py
@@ -6,8 +6,8 @@ class Config:
     CSRF_ENABLED = True
     SECRET = os.getenv('SECRET')
     # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/challenge_3_api"
-    # SQLALCHEMY_DATABASE_URI = os.environ['HEROKU_POSTGRESQL_PURPLE_URL']
+    # SQLALCHEMY_DATABASE_URI = "postgresql://localhost/challenge_3_api"
+    SQLALCHEMY_DATABASE_URI = os.environ['HEROKU_POSTGRESQL_PURPLE_URL']
 
 
 
