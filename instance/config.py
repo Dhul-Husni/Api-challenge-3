6d730842e3e091fab9401dc4edd683fdad6c1c21
sir3n-sn <sir3n.sn@gmail.com>
2017-12-03 15:22:09 +0300

    Fixed ignore instance

---
+++ instance/config.py
@@ -0,0 +1,40 @@
+import os
+
+class Config:
+    """parent configuration file"""
+    DEBUG = False
+    CSRF_ENABLED = True
+    SECRET = os.getenv('SECRET')
+    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/challenge_3_api"
+
+
+class DevelopmentConfig(Config):
+    """configuration for development"""
+    DEBUG = True
+
+class TestingConfig(Config):
+    """configuration for testing with a separate testing database
+    """
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/test_db'
+    DEBUG = True
+
+class StagingConfig(Config):
+    """config for staging
+    """
+class ProductionConfig(Config):
+    """ config for production
+    """
+    DEBUG = False
+    TESTING = False
+
+app_config = {
+    'development': DevelopmentConfig,
+    'staging': StagingConfig,
+    'production': ProductionConfig,
+    'testing': TestingConfig,
+}
+
+
+
