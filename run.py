dc5ec2069ca20f5069dd01a7be2b714ef98f3c36
sir3n-sn <sir3n.sn@gmail.com>
2018-01-06 21:11:44 +0300

    [chore] Update host for heroku

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ app = create_app(config_name)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
     # app.run(port=port)
 
 
